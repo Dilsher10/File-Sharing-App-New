25369701d2860b84c77a1504de84d7e09c48a39a
dilsher10 <dilsher.dahri10@gmail.com>
2026-01-17 00:07:55 +0500

    changes

--- app/page.js
+++ app/page.js
@@ -1,14 +1,1 @@
-"use client"
 
-import Image from "next/image";
-import Header from "./_components/Header";
-import Hero from "./_components/Hero";
-
-export default function Home() {
-  return (
-    <div>
-      <Header/>
-      <Hero/>
-    </div>
-  );
-}
